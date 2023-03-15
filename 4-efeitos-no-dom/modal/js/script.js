404083e9b274a797f2bdb5394a8d1dc7db1b4376
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-15 08:37:11 -0300

    Modal

---
+++ 4-efeitos-no-dom/modal/js/script.js
@@ -0,0 +1,11 @@
+import initSmoothScroll from './modules/smooth-scroll.js';
+import initScrollAnimation from './modules/anima-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js';
+
+initSmoothScroll();
+initScrollAnimation();
+initTabNav();
+initAccordion();
+initModal();
