e9fdd1df5abb8435f280f1da03924e09626e22f1
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-17 08:46:05 -0300

    Menu Mobile

---
+++ 4-efeitos-no-dom/menu-mobile/js/script.js
@@ -0,0 +1,17 @@
+import initSmoothScroll from './modules/smooth-scroll.js';
+import initScrollAnimation from './modules/anima-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+
+initSmoothScroll();
+initScrollAnimation();
+initTabNav();
+initAccordion();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
